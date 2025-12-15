99915c31fdf65d286bedf51c86a58b13773b646b
ahsan <decent.ahsan@gmail.com>
2025-12-16 01:14:28 +0500

    Zoom out radar by default
    Changed initial scale from 1.0 to 0.75 to show more of the radar
    at once. Users can still zoom in/out using scroll or +/- keys.

--- radar.js
+++ radar.js
@@ -10,7 +10,7 @@ class TechRadar {
         this.searchTerm = '';
 
         // Zoom and pan
-        this.scale = 1;
+        this.scale = 0.75;
         this.translateX = 0;
         this.translateY = 0;
         this.isDragging = false;
